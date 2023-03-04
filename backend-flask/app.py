588921259125ede5fb102b419f20743f24241e96
Olalekan Fadele <keensono@gmail.com>
2023-03-04 01:23:07 +0000

    Add segments

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -37,7 +37,6 @@ provider.add_span_processor(processor)
 # X-RAY -------------
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 
 
@@ -49,6 +48,9 @@ tracer = trace.get_tracer(__name__)
 
 
 app = Flask(__name__)
+
+# X-RAY -------------
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb --------
 # Initialize automatic instrumentation with Flask
